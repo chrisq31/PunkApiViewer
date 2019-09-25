cb4b25d004971bcfb36e1d8d5a5fe398b9de2e10
chrisquinncreative <quinn.uk@gmail.com>
2019-09-25 10:30:22 +0100

    Updates

---
+++ precache-manifest.02aa9ef1bad68e8ff128110556358be7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac29b724ba2bc33ce3002f9fca372caf",
+    "url": "/chrisq31/PunkApiViewer/index.html"
+  },
+  {
+    "revision": "0afd6233b8c4ce011f23",
+    "url": "/chrisq31/PunkApiViewer/static/css/main.6b66ec84.chunk.css"
+  },
+  {
+    "revision": "19d69ea5bead786e5b3e",
+    "url": "/chrisq31/PunkApiViewer/static/js/2.8ee60947.chunk.js"
+  },
+  {
+    "revision": "0afd6233b8c4ce011f23",
+    "url": "/chrisq31/PunkApiViewer/static/js/main.7fa09103.chunk.js"
+  },
+  {
+    "revision": "81c2be808eb961e8af8b",
+    "url": "/chrisq31/PunkApiViewer/static/js/runtime~main.230d656c.js"
+  },
+  {
+    "revision": "981a767244e7a00dcea64c4bcec832d3",
+    "url": "/chrisq31/PunkApiViewer/static/media/leaguegothic-regular-webfont.981a7672.woff2"
+  },
+  {
+    "revision": "ba4cbc9ab63d19bd84022dc81cb5e034",
+    "url": "/chrisq31/PunkApiViewer/static/media/leaguegothic-regular-webfont.ba4cbc9a.woff"
+  },
+  {
+    "revision": "a9dbc3343934c68fdc3043b75420eb34",
+    "url": "/chrisq31/PunkApiViewer/static/media/logo.a9dbc334.svg"
+  }
+]);
